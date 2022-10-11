90626d7271393fd5a05c910392d3c4f57bd7621a
sophie <sophie.human.k@gmail.com>
2022-10-11 14:44:51 +0900

    test5 delete 1fr

--- src/pages/test5.vue
+++ src/pages/test5.vue
@@ -12,13 +12,13 @@
       "
     >
       <div style="
-        width: 100%
-        height: 100%
-        background-color: white
-        display: grid
-        grid-template-rows: 88px 300px
-        padding-bottom: 121px"
-      >
+        width: 100%;
+        height: 100%;
+        background-color: white;
+        display: grid;
+        grid-template-rows: 88px 300px;
+        padding-bottom: 121px;
+      ">
         <div style="width: 100%; height: 100%; background-color: yellow;">title</div>
         <div
           style="

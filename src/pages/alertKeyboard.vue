dbd22f6d04e4c9011bf7db186b56d6fd63090702
sophie <sophie.human.k@gmail.com>
2022-10-12 16:03:39 +0900

    ADD alertKeyboard

---
+++ src/pages/alertKeyboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <button @click='alert("alert")'>Alert</button>
+      <q-input label="keyboard" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "alertKeyboard",
+  methods: {
+    alert (message) {
+      alert(message)
+    }
+  }
+}
+</script>
+
+<style>
+[data-notify] {
+  z-index: 9999 !important;
+}
+</style>
